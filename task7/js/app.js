f2c768188f97dd168fdd05359c62856727f25bab
G0V0RUN <alexandrvasilev12rus@gmail.com>
2021-02-11 21:54:33 +0300

    Задание 7 | accordeon

--- task7/js/app.js
+++ task7/js/app.js
@@ -14,12 +14,25 @@ function setToggleAccordion() {
     let accordionHeaders = document.querySelectorAll(".accordion_header");
     accordionHeaders.forEach(accordionHeader => {
         accordionHeader.addEventListener("click", event => {
-            accordionHeader.classList.toggle("active");
+            
             let toggleIcon = accordionHeader.querySelector(".toggle_plus");
             console.log(toggleIcon);
-            toggleIcon.classList.toggle("active");
+            let currentActiveAccordionHeader = document.querySelector(".accordion_header.active");
+            let currentToggleIcon = document.querySelector(".toggle_plus.active");
+            let currentAccordionText = document.querySelector(".accordion_block_text.active");
             let accordionText = accordionHeader.nextElementSibling;
-            console.log(accordionText);
+            
+            if(currentActiveAccordionHeader && currentActiveAccordionHeader !== accordionHeader) {
+                currentActiveAccordionHeader.classList.toggle("active");
+                currentToggleIcon.classList.toggle("active");
+                currentAccordionText.classList.toggle("active");
+                currentActiveAccordionHeader.nextElementSibling.style.maxHeight = 0;
+            }
+            accordionHeader.classList.toggle("active");
+            console.log(toggleIcon);
+            toggleIcon.classList.toggle("active");
+            accordionText.classList.toggle("active");
+            
             if(accordionHeader.classList.contains("active")) {
                 accordionText.style.maxHeight = accordionText.scrollHeight + "px";
             } else {
