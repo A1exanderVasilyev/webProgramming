40a61e20b20576b3f9a4588c547334efa01752a5
G0V0RUN <alexandrvasilev12rus@gmail.com>
2021-03-09 17:20:46 +0300

    hide menu over popup

--- task8(1)/js/popup.js
+++ task8(1)/js/popup.js
@@ -2,18 +2,24 @@ let popup = document.getElementById('popup');
 let popupClose = document.querySelector('.popup_close');
 let popupButtonForm = document.getElementById('popup_button');
 let popupButtonEnrollment = document.querySelectorAll('.enrollment_button');
+let menu = document.querySelector('.menu_burger');
 console.log(popupButtonForm);
 console.log(popupButtonEnrollment);
 
 
 popupButtonForm.onclick = function() {
     popup.classList.add('open');
+    menu.classList.add('hidden');
 }
 
 popupButtonEnrollment.forEach(popupButtonEnrollment => {
-    popupButtonEnrollment.addEventListener('click', () => popup.classList.add('open'));
+    popupButtonEnrollment.addEventListener('click', function() {
+        popup.classList.add('open');
+        menu.classList.add('hidden');
+    })
 })
 
 popupClose.onclick = function() {
     popup.classList.remove('open');
+    menu.classList.remove('hidden');
 }
