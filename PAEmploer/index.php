eb2fc0ff7e1518fbdc44cefabe1760fe99ddda89
G0V0RUN <alexandrvasilev12rus@gmail.com>
2021-05-04 16:25:07 +0300

    верстка

---
+++ PAEmploer/index.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+if ($_SESSION['user']) {
+    header('Location: profile.php');
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Личный кабинет работодателя</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat&family=Sarala&display=swap&family=Open+Sans&display=swap"
+        rel="stylesheet">
+    <link href="source/css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- Форма авторизации -->
+
+    <form action="account/signin.php" method="post" class="authorization__form">
+        <label>Логин</label>
+        <input type="text" name="login" placeholder="Введите свой логин">
+        <label>Пароль</label>
+        <input type="password" name="password" placeholder="Введите пароль">
+        <button type="submit">Войти</button>
+        <p>
+            У вас нет аккаунта? - <a href="/register.php">зарегистрируйтесь</a>!
+        </p>
+        <?php
+            if ($_SESSION['message']) {
+                echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
+            }
+            unset($_SESSION['message']);
+        ?>
+    </form>
+
+</body>
+</html>
